1069b8750039d6ed0379ee0b3b7ca504e8c34c7e
Pavlo Shchur <pavloshchur@gmail.com>
2022-08-30 23:07:34 +0300

    Basic styling for video buttons is added.

--- www/index.js
+++ www/index.js
@@ -1,1 +1,9 @@
-alert('index.js is invoked!');
+function turnOnVideo() {
+    document.getElementById('turnOnCamera').classList.add('display-none');
+    document.getElementById('turnOffCamera').classList.remove('display-none');
+}
+
+function turnOffVideo() {
+    document.getElementById('turnOffCamera').classList.add('display-none');
+    document.getElementById('turnOnCamera').classList.remove('display-none');
+}
