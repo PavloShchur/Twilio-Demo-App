ee2b3c42647f0cee6581ba93155f9680884281cd
Pavlo Shchur <pavloshchur@gmail.com>
2022-08-31 11:51:04 +0300

    body parser is removed.

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
 var pg = require('pg');
 
 var app = express();
 
 app.use(express.static('www'));
 app.use(express.static(path.join('www', 'build')));
-
-app.use(bodyParser.json());
 
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dreamhouse';
 
@@ -22,5 +19,3 @@ client.connect();
 var port = process.env.PORT || 8200;
 
 app.listen(port);
-
-//console.log('Listening at: http://localhost:' + port);
