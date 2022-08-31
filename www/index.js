c999775ea2108e2a3512e5b702d84e9cbdd9e7be
Pavlo Shchur <pavloshchur@gmail.com>
2022-08-31 12:33:29 +0300

    Twilio JS library is added.

--- www/index.js
+++ www/index.js
@@ -1,3 +1,15 @@
+(function () {
+
+    console.log('init()');
+
+    const connect : Twilio.Video.connect;
+    console.log('init(), connect:', connect);
+
+    const createLocalAudioTrack : Twilio.Video.createLocalAudioTrack;
+    const createLocalVideoTrack : Twilio.Video.createLocalVideoTrack;
+    const localDataTrack : Twilio.Video.LocalDataTrack;
+})();
+
 function turnOnVideo() {
     document.getElementById('turnOnCamera').classList.add('display-none');
     document.getElementById('turnOffCamera').classList.remove('display-none');
